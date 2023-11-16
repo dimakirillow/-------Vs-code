363e98d9ca4a3c776077b4437625c1b5e6e36628
dimakirillow <kirilloff.mity2013@gmail.com>
2023-11-16 15:44:37 +0300

    Объявите следующую функцию TestFunction:
    Функция TestFunction вызывает другую функцию SubTestFunction, уже объявленную
    Код описать в одной строке без лишних пробелов и комментариев

--- 1.js
+++ 1.js
@@ -1,1 +1,8 @@
-scopes.onEdit(RegTest);
+let SubTestFunction = function(){
+    console.log("Привет я SubTestFunction, меня вызвала функиция TestFunction")
+}
+
+let TestFunction = ()=>{
+    SubTestFunction()
+}
+TestFunction()
