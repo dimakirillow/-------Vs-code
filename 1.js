8225bf44e3c853f8287a8cf482e63645aad49548
dimakirillow <kirilloff.mity2013@gmail.com>
2023-11-16 15:47:09 +0300

    Функция RegTest должна автоматически отрабатывать при взятии документа на редактирование. Вставьте пропущенные значения в полях.

--- 1.js
+++ 1.js
@@ -1,8 +1,1 @@
-let SubTestFunction = function(){
-    console.log("Привет я SubTestFunction, меня вызвала функиция TestFunction")
-}
-
-let TestFunction = ()=>{
-    SubTestFunction()
-}
-TestFunction()
+scopes.onEdit(RegTest);
